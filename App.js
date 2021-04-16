6bd8bcbf0f792c5e8d3150942a26e922188e5ca5
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-16 13:12:46 +0600

    game screen functional, wrong input bug remains to be fixed

--- App.js
+++ App.js
@@ -4,12 +4,18 @@ import { StyleSheet, View } from "react-native";
 import Header from "./Components/Header";
 import StartGameScreen from "./Screens/StartGameScreen";
 import GameScreen from "./Screens/GameScreen";
+import GameOverScreen from "./Screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = (numberOfRounds) => {
+    setGuessRounds(numberOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
--- Screens/GameScreen.js
+++ Screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 import NumberContainer from "../Components/NumberContainer";
 import Card from "../Components/Card";
@@ -7,7 +7,7 @@ import Card from "../Components/Card";
 const generateRandomNumber = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  const randomNumber = Math.floor(Math.random() * (min - max)) + min;
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
   if (randomNumber === exclude) {
     return generateRandomNumber(min, max, exclude);
   } else {
@@ -19,13 +19,49 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomNumber(1, 100, props.userChoice)
   );
+
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  useEffect(() => {
+    if (currentGuess === props.userChoice) {
+    }
+  });
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't cheat...!!!", "You know this is wrong!", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomNumber(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Ai Gussed: </Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="Lower" onPress={() => {}} />
-        <Button title="Greater" onPress={() => {}} />
+        <Button title="Lower" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="Greater"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
       </Card>
     </View>
   );
