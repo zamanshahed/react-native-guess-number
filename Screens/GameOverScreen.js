b3dd7d22eda5296bb3abbf21ec1b23ae0789ec95
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-17 22:55:45 +0600

    gameover screen styled a bit

--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+import Colors from "../Constants/Colors";
+
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The game is over!</Text>
-      <Text>Score: {props.roundsNumber}</Text>
-      <Text>Actual Guess: {props.userNumberWas}</Text>
-      <Button title="new game" onPress={props.onRestart} />
+      <Text style={styles.text}>The game is over!</Text>
+      <Text style={styles.text}>Score: {props.roundsNumber}</Text>
+      <Text style={styles.text}>Actual Guess: {props.userNumberWas}</Text>
+      <Button
+        style={styles.button}
+        title="new game"
+        onPress={props.onRestart}
+      />
     </View>
   );
 };
@@ -17,6 +23,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "red",
+    fontWeight: "bold",
+    padding: 3,
+  },
+  button: {
+    padding: 6,
+    margin: 15,
   },
 });
 
