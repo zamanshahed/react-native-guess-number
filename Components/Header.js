fc8126bb17c618dd4d7b929c10bdb753b2b13e92
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-27 13:58:04 +0600

    depending platforms android/ios different set of header styling applied

--- Components/Header.js
+++ Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 import Colors from "../Constants/Colors";
 import TitleText from "../Components/TitleText";
@@ -17,12 +17,14 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 78,
     paddingTop: 25,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
     alignItems: "center",
     justifyContent: "center",
+    borderBottomColor: Platform.OS === "ios" ? "#ccc" : "transparent",
+    borderBottomWidth: Platform.OS === "ios" ? 1 : 0,
   },
   headerTitle: {
-    color: "#fff",
+    color: Platform.OS === "ios" ? Colors.primary : "white",
     fontSize: 28,
   },
 });
